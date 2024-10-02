d277e1666efb042e3b3c9de85c5010d803febedc
semanticdata <contact@miguelpimentel.do>
2024-10-02 15:46:16 -0500

    enable breadcrumbs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,4 +1,4 @@
-import {PageLayout, SharedLayout} from "./quartz/cfg"
+import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
 // components shared across all pages
@@ -20,7 +20,12 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [Component.ArticleTitle()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    // Component.ContentMeta(),
+    // Component.TagList(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -32,31 +37,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    // Component.MobileOnly(Component.Explorer()),
   ],
-
-  // beforeBody: [
-  //   Component.Breadcrumbs(),
-  //   Component.ArticleTitle(),
-  //   Component.ContentMeta(),
-  //   Component.TagList(),
-  // ],
-  // left: [
-  //   Component.PageTitle(),
-  //   Component.MobileOnly(Component.Spacer()),
-  //   Component.Search(),
-  //   Component.Darkmode(),
-  //   Component.DesktopOnly(Component.Explorer()),
-  // ],
-  // right: [
-  //   Component.Graph(),
-  //   Component.DesktopOnly(Component.TableOfContents()),
-  //   Component.Backlinks(),
-  // ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.ArticleTitle()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -65,14 +52,4 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
-
-  // beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  // left: [
-  //   Component.PageTitle(),
-  //   Component.MobileOnly(Component.Spacer()),
-  //   Component.Search(),
-  //   Component.Darkmode(),
-  //   Component.DesktopOnly(Component.Explorer()),
-  // ],
-  // right: [],
 }
